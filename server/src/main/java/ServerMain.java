bf902c8a7ba7c0b7729db36a0236aaf9d7572e66
Jeffrey Grover <javagroover@gmail.com>
2026-02-16 00:29:02 -0700

    Setup Server Main

--- server/src/main/java/ServerMain.java
+++
@@ -1,9 +0,0 @@
-import chess.ChessGame;
-import chess.ChessPiece;
-
-public class ServerMain {
-    public static void main(String[] args) {
-        var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
-        System.out.println("♕ 240 Chess Server: " + piece);
-    }
-}
