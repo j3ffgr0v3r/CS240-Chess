4996547176b369bc7d2c58a8fdd950aecb4aa5ea
Jeffrey Grover <javagroover@gmail.com>
2026-03-01 22:24:50 -0700

    Removed authorization checking from GameService

--- server/src/main/java/service/GameService.java
+++ server/src/main/java/service/GameService.java
@@ -1,27 +1,17 @@
 package service;
 
-import dataaccess.Auth.AuthDAO;
 import dataaccess.Game.GameDAO;
-import model.AuthData;
 import requestsandresults.ListGames.ListGamesResult;
 
 public class GameService {
 
     GameDAO gameDAO;
-    AuthDAO authDAO;
 
-    public GameService(GameDAO gameDAO, AuthDAO authDAO) {
+    public GameService(GameDAO gameDAO) {
         this.gameDAO = gameDAO;
-        this.authDAO = authDAO;
     }
 
     public ListGamesResult listGames(String authToken) {
-        AuthData authData = authDAO.getSession(authToken);
-
-        if (authData == null) {
-            return new ListGamesResult("Error: unauthorized", null);
-        }
-
         return new ListGamesResult(null, gameDAO.getAllGames());
     }
 
