88b1cf6fab81e1a77e6a5dc4e2d91bb8ebbff063
Jeffrey Grover <javagroover@gmail.com>
2026-03-02 02:20:10 -0700

    Refactored out Pawn moves to it's own function

--- shared/src/main/java/chess/PieceMovesCalculator.java
+++ shared/src/main/java/chess/PieceMovesCalculator.java
@@ -13,14 +13,8 @@ public class PieceMovesCalculator {
 
     public static Collection<ChessMove> calculateMoves(ChessBoard board, ChessPosition position, ChessGame.TeamColor color,
             List<Behavior> behaviors) {
-                
-        List<ChessMove> moves = new ArrayList<>();
 
-        List<ChessPiece.PieceType> promotions = new ArrayList<>(Arrays.asList(
-                ChessPiece.PieceType.QUEEN,
-                ChessPiece.PieceType.BISHOP,
-                ChessPiece.PieceType.KNIGHT,
-                ChessPiece.PieceType.ROOK));
+        List<ChessMove> moves = new ArrayList<>();
 
         for (Behavior behavior : behaviors) {
             for (int i = 1; behavior.distance == -1 || i < behavior.distance + 1; i++) {
@@ -34,21 +28,10 @@ public class PieceMovesCalculator {
                 ChessPosition newPosition = new ChessPosition(newRow, newColumn);
                 ChessPiece tile = board.getPiece(newPosition);
 
-                if (tile == null || tile.getTeamColor() != color) {
-                    // Split Behavior (Pawn)
+                if (tile == null || tile.getTeamColor() != color) { 
                     if (board.getPiece(position).getPieceType() == ChessPiece.PieceType.PAWN) {
-                        if ((position.getColumn() == newColumn && tile == null)
-                                || (position.getColumn() != newColumn && tile != null)) {
-
-                            // Promotion
-                            if (newRow == 1 || newRow == 8) {
-                                for (ChessPiece.PieceType promotion : promotions) {
-                                    moves.add(new ChessMove(position, newPosition, promotion));
-                                }
-                            } else {
-                                moves.add(new ChessMove(position, newPosition, null));
-                            }
-                        }
+                        // Split Behavior (Pawn)
+                        moves.addAll(pawnMoves(position, newPosition, tile));
 
                     } else {
                         // Attack and move together
@@ -61,6 +44,34 @@ public class PieceMovesCalculator {
                 } else {
                     break;
                 }
+            }
+        }
+
+        return moves;
+    }
+
+    private static Collection<ChessMove> pawnMoves(ChessPosition position, ChessPosition newPosition, ChessPiece tile) {
+        List<ChessPiece.PieceType> promotions = new ArrayList<>(Arrays.asList(
+                ChessPiece.PieceType.QUEEN,
+                ChessPiece.PieceType.BISHOP,
+                ChessPiece.PieceType.KNIGHT,
+                ChessPiece.PieceType.ROOK));
+
+        int newRow = newPosition.getRow();
+        int newColumn = newPosition.getColumn();
+
+        List<ChessMove> moves = new ArrayList<>();
+
+        if ((position.getColumn() == newColumn && tile == null)
+                || (position.getColumn() != newColumn && tile != null)) {
+
+            // Promotion
+            if (newRow == 1 || newRow == 8) {
+                for (ChessPiece.PieceType promotion : promotions) {
+                    moves.add(new ChessMove(position, newPosition, promotion));
+                }
+            } else {
+                moves.add(new ChessMove(position, newPosition, null));
             }
         }
 
