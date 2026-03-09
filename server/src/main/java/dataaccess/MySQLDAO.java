ac8a17e07a28c308b865ac25e649c1d5a5b18989
Jeffrey Grover <javagroover@gmail.com>
2026-03-08 21:21:26 -0600

    Updated MySQLDAO to support an executeUpdate method

--- server/src/main/java/dataaccess/MySQLDAO.java
+++ server/src/main/java/dataaccess/MySQLDAO.java
@@ -1,9 +1,33 @@
 package dataaccess;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import static java.sql.Statement.RETURN_GENERATED_KEYS;
+
+import com.google.gson.Gson;
 
 public abstract class MySQLDAO {
+
+    protected boolean executeUpdate(String statement, Object... params) throws DataAccessException {
+        try (Connection conn = DatabaseManager.getConnection()) {
+            try (PreparedStatement ps = conn.prepareStatement(statement, RETURN_GENERATED_KEYS)) {
+                for (int i = 0; i < params.length; i++) {
+                    Object param = params[i];
+                    if (param instanceof String p) ps.setString(i + 1, p);
+                    else if (param instanceof Integer p) ps.setInt(i + 1, p);
+                    else ps.setString(i + 1, new Gson().toJson(param));
+                }
+                ps.executeUpdate();
+
+                ResultSet rs = ps.getGeneratedKeys();
+                return rs.next();
+            }
+        } catch (SQLException e) {
+            throw new DataAccessException(String.format("unable to update database: %s, %s", statement, e.getMessage()));
+        }
+    }
 
     protected void configureDatabase(String[] createStatements) throws DataAccessException {
         DatabaseManager.createDatabase();
