1bba17dacf2a0058547fcac41ec32278046dfdd9
Jeffrey Grover <javagroover@gmail.com>
2026-03-10 19:25:16 -0600

    Refactored SQLDAOs to use one function to execute SQL Code

--- server/src/main/java/dataaccess/MySQLDAO.java
+++ server/src/main/java/dataaccess/MySQLDAO.java
@@ -4,39 +4,12 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import static java.sql.Statement.RETURN_GENERATED_KEYS;
 
 import com.google.gson.Gson;
 
 public abstract class MySQLDAO {
 
-    protected int executeUpdate(String statement, Object... params) throws DataAccessException {
-        try (Connection conn = DatabaseManager.getConnection()) {
-            try (PreparedStatement ps = conn.prepareStatement(statement, RETURN_GENERATED_KEYS)) {
-                for (int i = 0; i < params.length; i++) {
-                    Object param = params[i];
-                    if (param instanceof String p)
-                        ps.setString(i + 1, p);
-                    else if (param instanceof Integer p)
-                        ps.setInt(i + 1, p);
-                    else
-                        ps.setString(i + 1, new Gson().toJson(param));
-                }
-                ps.executeUpdate();
-
-                ResultSet rs = ps.getGeneratedKeys();
-                if (rs.next()) {
-                    return rs.getInt(1);
-                }
-
-                return 0;
-            }
-        } catch (SQLException e) {
-            throw new DataAccessException(String.format("unable to update database: %s, %s", statement, e.getMessage()));
-        }
-    }
-
-    public ResultSet getFromTable(String statement, Object... params) throws DataAccessException {
+    public ResultSet executeSQL(String statement, Object... params) throws DataAccessException {
         try (Connection conn = DatabaseManager.getConnection()) {
             try (PreparedStatement ps = conn.prepareStatement(statement)) {
                 for (int i = 0; i < params.length; i++) {
