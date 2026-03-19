719a29185f16a6dda23b2611c0a6b343ff505bf3
Jeffrey Grover <javagroover@gmail.com>
2026-03-18 19:09:52 -0600

    Added basic command validation

--- client/src/main/java/ui/ChessClient.java
+++ client/src/main/java/ui/ChessClient.java
@@ -3,38 +3,44 @@ package ui;
 import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
-import java.util.function.Function;
 
 import client.ServerCommunicationFailure;
 
 public class ChessClient {
     private final ServerFacade server;
+
     private enum UIState {
-        PRELOGIN,
-        POSTLOGIN,
-        GAMEPLAY
+        PRELOGIN, POSTLOGIN, GAMEPLAY
     }
 
-    private record MenuOption(String usage, Function<Object, Object> func) {}
-    private final List<MenuOption> preLoginMenu = List.of(new MenuOption("register <USERNAME> <PASSWORD> <EMAIL> - register a new account", null),
-                                                    new MenuOption("login <USERNAME> <PASSWORD> - sign in to play", null),
-                                                    new MenuOption("quit - quit the program", null),
-                                                    new MenuOption("help - see more information about this menu", null));
+    @FunctionalInterface
+    interface MultiStringConsumer {
+        void accept(String... strings);
+    }
+
+    private record MenuOption(String usage, MultiStringConsumer func) {
+    }
+
+    private final List<MenuOption> preLoginMenu = List.of(
+            new MenuOption("register <USERNAME> <PASSWORD> <EMAIL> - register a new account", (params) -> register(params[0], params[1], params[2])),
+            new MenuOption("login <USERNAME> <PASSWORD> - sign in to play", null),
+            new MenuOption("quit - quit the program", null),
+            new MenuOption("help - see more information about this menu", null));
 
     private final List<MenuOption> postLoginMenu = List.of(new MenuOption("create <NAME> - start a new game", null),
-                                                    new MenuOption("list - list all active games", null),
-                                                    new MenuOption("join <ID> [WHITE|BLACK] - join a game as selected team", null),
-                                                    new MenuOption("observe <ID> - spectate an active game", null),
-                                                    new MenuOption("logout - logout from the current user", null),
-                                                    new MenuOption("quit - quit the program", null),
-                                                    new MenuOption("help - see more information about this menu", null));
+            new MenuOption("list - list all active games", null),
+            new MenuOption("join <ID> [WHITE|BLACK] - join a game as selected team", null),
+            new MenuOption("observe <ID> - spectate an active game", null),
+            new MenuOption("logout - logout from the current user", null),
+            new MenuOption("quit - quit the program", null),
+            new MenuOption("help - see more information about this menu", null));
 
     private final List<MenuOption> gameplayMenu = List.of(new MenuOption("leave - leave this game", null));
 
     private final Map<UIState, List<MenuOption>> stateMenuOptions = Map.of(UIState.PRELOGIN, preLoginMenu,
-                                                                            UIState.POSTLOGIN, postLoginMenu,
-                                                                            UIState.GAMEPLAY, gameplayMenu);
-    
+            UIState.POSTLOGIN, postLoginMenu,
+            UIState.GAMEPLAY, gameplayMenu);
+
     public ChessClient(final String serverUrl) throws ServerCommunicationFailure {
         server = new ServerFacade(serverUrl);
     }
@@ -47,6 +53,11 @@ public class ChessClient {
                 printMenu(uiState);
                 final String line = scanner.nextLine();
                 System.out.println();
+                if (validateInput(uiState, line)) {
+                    System.out.println("That's really a command! Yay!");
+                } else {
+                    System.out.println("Bruh, that's not valid");
+                }
             }
         }
     }
@@ -56,5 +67,14 @@ public class ChessClient {
             System.out.println(option.usage);
         }
         System.out.println();
+    }
+
+    private boolean validateInput(final UIState state, String line) {
+        MenuOption command = stateMenuOptions.get(state).stream().filter(option -> option.usage().split("\\s+")[0].equals(line.split("\\s+")[0])).findFirst().orElse(null);
+        return command != null;
+    }
+
+    private void register(String username, String email, String password) {
+
     }
 }
