5007cba886f31d72631faf24e2c8cbf1c816edc1
Jeffrey Grover <javagroover@gmail.com>
2026-03-21 16:07:11 -0600

    Added ServerFacadeTests

---
+++ client/src/test/java/client/ServerFacadeTest.java
@@ -0,0 +1,157 @@
+package client;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.jupiter.api.AfterAll;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import model.GameData;
+import model.exceptions.HTTPException;
+import server.Server;
+
+public class ServerFacadeTest {
+
+    private static Server server;
+    static ServerFacade facade;
+    private static int port;
+
+    @BeforeAll
+    public static void init() {
+        server = new Server();
+        port = server.run(0);
+        System.out.println("Started test HTTP server on " + port);
+    }
+
+    @AfterAll
+    static void stopServer() {
+        server.stop();
+    }
+
+    @BeforeEach
+    void clearDatabase() {
+        try {
+            facade = new ServerFacade("http://localhost:" + port);
+        } catch (ServerCommunicationFailure e) {
+            throw new RuntimeException(e.getMessage());
+        }
+        try {
+            facade.clearDatabase();
+        } catch (HTTPException e) {
+            throw new RuntimeException(e.getMessage());
+        }
+        
+    }
+
+    @Test
+    void testCreateGame() {
+        try {
+            facade.register("username", "email", "password");
+        } catch (HTTPException e) {
+            throw new RuntimeException(e.getMessage());
+        }
+        
+        assertDoesNotThrow(() -> facade.createGame("newGame"));
+    }
+
+    @Test
+    void testCreateGameUnauthenticated() {
+        HTTPException exception = assertThrows(HTTPException.class, () -> facade.createGame("newGame"));
+        assertTrue(exception.getMessage().toLowerCase().contains("unauthorized"));
+    }
+
+    @Test
+    void testListGames() {
+        List<String> gameNames = Arrays.asList("Game1", "Game2", "Game3");
+        try {
+            facade.register("username", "email", "password");
+            for (String game : gameNames) {
+                facade.createGame(game);
+            }
+            List<GameData> games = facade.listGames();
+            for (GameData game : games) {
+                assertTrue(gameNames.contains(game.gameName()));
+            }
+        } catch (HTTPException e) {
+            throw new RuntimeException(e.getMessage());
+        }
+    }
+
+    @Test
+    void testListGamesNoneFound() {
+        try {
+            facade.register("username", "email", "password");
+            List<GameData> games = facade.listGames();
+            assertTrue(games.isEmpty());
+        } catch (HTTPException e) {
+            throw new RuntimeException(e.getMessage());
+        }
+    }
+
+    @Test
+    void testLogin() {
+        try {
+            facade.register("username", "email", "password");
+        } catch (HTTPException e) {
+            throw new RuntimeException(e.getMessage());
+        }
+        assertDoesNotThrow(() -> facade.login("username", "password"));
+    }
+
+    @Test
+    void testLoginUnauthorized() {
+        HTTPException exception = assertThrows(HTTPException.class, () -> facade.login("username", "password"));
+        assertTrue(exception.getMessage().toLowerCase().contains("unauthorized"));
+    }
+
+    @Test
+    void testLogout() {
+        try {
+            facade.register("username", "email", "password");
+        } catch (HTTPException e) {
+            throw new RuntimeException(e.getMessage());
+        }
+        assertDoesNotThrow(() -> facade.logout());
+        HTTPException exception = assertThrows(HTTPException.class, () -> facade.listGames());
+        assertTrue(exception.getMessage().toLowerCase().contains("unauthorized"));
+    }
+
+    @Test
+    void testJoinGame() {
+        int gameID;
+        try {
+            facade.register("username", "email", "password");
+            gameID = facade.createGame("gameName");
+        } catch (HTTPException e) {
+            throw new RuntimeException(e.getMessage());
+        }
+        assertDoesNotThrow(() -> facade.joinGame(gameID, "WHITE"));
+    }
+
+    @Test
+    void testJoinGameDoesNotExist() {
+        try {
+            facade.register("username", "email", "password");
+        } catch (HTTPException e) {
+            throw new RuntimeException(e.getMessage());
+        }
+        HTTPException exception = assertThrows(HTTPException.class, () -> facade.joinGame(0, "BLACK"));
+        assertTrue(exception.getMessage().toLowerCase().contains("bad request"));
+    }
+
+    @Test
+    void testRegister() {
+        assertDoesNotThrow(() -> facade.register("username", "email", "password"));
+    }
+
+    @Test
+    void testRegisterBadRequest() {
+        HTTPException exception = assertThrows(HTTPException.class, () -> facade.register(null, null, null));
+        assertTrue(exception.getMessage().toLowerCase().contains("bad request"));
+    }
+}
