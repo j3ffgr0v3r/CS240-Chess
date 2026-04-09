6087e52e131c909e22ab306b2830d8b72d30086a
Jeffrey Grover <javagroover@gmail.com>
2026-04-09 02:56:17 -0600

    Added MakeMoveCommand

---
+++ shared/src/main/java/websocket/commands/MakeMoveCommand.java
@@ -0,0 +1,18 @@
+package websocket.commands;
+
+import chess.ChessMove;
+
+public class MakeMoveCommand extends UserGameCommand {
+
+    ChessMove move;
+
+    public MakeMoveCommand(ChessMove move, String authToken, Integer gameID) {
+        super(CommandType.MAKE_MOVE, authToken, gameID);
+        this.move = move;
+    }
+
+    public ChessMove getMove() {
+        return move;
+    }
+    
+}
